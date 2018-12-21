c7a7fe63a68c9ebf56b089cc7fd4d7904730007e
Till <me@deletescape.ch>
2018-12-21 15:24:59 +0100

    Fix first.py
    Closes #1

--- examples/1-syntax/first.py
+++ examples/1-syntax/first.py
@@ -4,4 +4,4 @@ print("This is my first python script")
 pi = 3.14159
 radius = 10
 circumference = pi * 2 * radius
-print("The circumference of a circle with radius " + radius + " is " + circumference)
+print("The circumference of a circle with radius " + str(radius) + " is " + str(circumference))
